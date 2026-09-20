039075461ca9bacef160ec233cea5a799bb54e16
Sebastian <sebastianrabanal34@gmail.com>
2026-09-20 14:02:36 -0500

    chore: eliminar config.example.js

--- config.example.js
+++
@@ -1,3 +0,0 @@
-// Copia este archivo y pégalo como config.js con tu API key de TMDB.
-// config.js NO se sube a GitHub (está en .gitignore) para proteger tu clave.
-const TMDB_API_KEY = "PEGA_AQUI_TU_API_KEY";
